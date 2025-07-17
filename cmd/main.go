d6335add0f9ff5f4d26241e6d395b91c9ef66271
iqbalpa <iqbalpahlevi07@gmail.com>
2025-07-17 09:37:50 +0700

    feat: use master password derived for key

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 	"govault/internal/crypto"
-	_ "govault/internal/crypto"
 )
 
 func main() {
-	fmt.Println("hi")
-	a, _ := crypto.EncryptAES("iqbalpahlevi")
+	masterPass := "iqbal"
+	salt := crypto.GenerateRandomSalt()
+	key, _ := crypto.DeriveKey(masterPass, salt)
+	a, _ := crypto.EncryptAES("iqbalpahlevi", key)
 	fmt.Println(a)
-	b, _ := crypto.DecryptAES(a)
+	b, _ := crypto.DecryptAES(a, key)
 	fmt.Println(b)
-	c, _ := crypto.DeriveKey("iqbalpahlevi")
+	c, _ := crypto.DeriveKey("iqbalpahlevi", salt)
 	fmt.Println(c)
 }
--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -6,37 +6,34 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
-	"os"
 
 	_ "govault/internal/utils"
 
 	"golang.org/x/crypto/scrypt"
 )
 
-var secretKey string
-var key []byte
-var aesgcm cipher.AEAD
-
-func init() {
-	secretKey = os.Getenv("SECRET_KEY")
-	key = []byte(secretKey)
-	if len(key) != 32 {
-		panic("Key length must be 32")
+func createAesgcm(keyStr string) cipher.AEAD {
+	key, err := base64.StdEncoding.DecodeString(keyStr)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
-	aesgcm, err = cipher.NewGCM(block)
+	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
+	return aesgcm
 }
 
 // EncryptAES encrypts a string using AES-GCM.
-func EncryptAES(text string) (string, error) {
+func EncryptAES(text, key string) (string, error) {
 	nonce := make([]byte, 12)
 	rand.Read(nonce)
+
+	aesgcm := createAesgcm(key)
 
 	plaintext := []byte(text)
 	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
@@ -47,7 +44,7 @@ func EncryptAES(text string) (string, error) {
 }
 
 // DecryptAES decrypts a string encrypted with AES-GCM.
-func DecryptAES(ciphertext string) (string, error) {
+func DecryptAES(ciphertext, key string) (string, error) {
 	data, err := base64.StdEncoding.DecodeString(ciphertext)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -55,6 +52,8 @@ func DecryptAES(ciphertext string) (string, error) {
 
 	nonce := data[:12]
 	cipher := data[12:]
+
+	aesgcm := createAesgcm(key)
 
 	plaintext, err := aesgcm.Open(nil, nonce, cipher, nil)
 	if err != nil {
@@ -65,9 +64,8 @@ func DecryptAES(ciphertext string) (string, error) {
 }
 
 // DeriveKey derives a key from a master password using scrypt.
-func DeriveKey(masterPass string) (string, error) {
-	salt := GenerateRandomSalt()
-	key, err := scrypt.Key([]byte(masterPass), salt, 32768, 8, 1, 256)
+func DeriveKey(masterPass string, salt []byte) (string, error) {
+	key, err := scrypt.Key([]byte(masterPass), salt, 32768, 8, 1, 32)
 	if err != nil {
 		panic(err)
 	}
