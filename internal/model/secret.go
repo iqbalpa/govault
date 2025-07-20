44d1d57b56e248992cb1385a44bd1533fe7d685c
iqbalpa <iqbalpahlevi07@gmail.com>
2025-07-20 16:12:50 +0700

    feat: use composite index name_username

--- internal/model/secret.go
+++ internal/model/secret.go
@@ -4,8 +4,8 @@ import "time"
 
 type Secret struct {
 	ID         string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	Name       string `gorm:"unique"`
-	Username   string
+	Name       string `gorm:"uniqueIndex:idx_name_username"`
+	Username   string `gorm:"uniqueIndex:idx_name_username"`
 	Ciphertext []byte // AES-encrypted password
 	Salt       []byte // Salt to derived the master password
 	Note       string
