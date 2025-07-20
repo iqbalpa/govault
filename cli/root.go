d268ce3e922c15c4b9e107cd8e94dad524cb1aa1
iqbalpa <iqbalpahlevi07@gmail.com>
2025-07-20 08:20:40 +0700

    feat: init cli

---
+++ cli/root.go
@@ -0,0 +1,85 @@
+package cli
+
+import (
+	"fmt"
+	"govault/internal/repository"
+	"govault/internal/service"
+	"govault/internal/utils"
+
+	"github.com/spf13/cobra"
+	"gorm.io/gorm"
+)
+
+var (
+	db        *gorm.DB
+	vaultRepo *repository.SecretRepository
+	vaultSvc  *service.VaultService
+	authRepo  *repository.AuthRepository
+	authSvc   *service.AuthService
+)
+
+var (
+	masterPass string
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "govault",
+		Short: "A secure local password manager",
+		Long:  "Govault is a secure local password manager using cryptographic encryption",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Govault -- Local Password Manager  v0.1.0")
+		},
+	}
+
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of Govault",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Govault v0.1.0")
+		},
+	}
+
+	initCmd = &cobra.Command{
+		Use:   "init",
+		Short: "Init master password for govault",
+		Long:  "Master password initialization to use govault",
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Master Password:", masterPass)
+			authSvc.InitMasterPass(masterPass)
+		},
+	}
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	initServices()
+
+	initCmd.PersistentFlags().StringVarP(&masterPass, "masterPass", "m", "", "the master password to initialize govault")
+
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(initCmd)
+}
+
+func initServices() {
+	if db != nil {
+		return // Already initialized
+	}
+
+	// Initialize database
+	db = utils.ConnectDb()
+	utils.MigrateDb(db)
+
+	// Initialize repositories
+	vaultRepo = repository.New(db)
+	authRepo = repository.NewAuthRepo(db)
+
+	// Initialize services
+	vaultSvc = service.New(*vaultRepo)
+	authSvc = service.NewAuthService(*authRepo)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,54 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"govault/internal/crypto"
-	"govault/internal/repository"
-	"govault/internal/service"
-	"govault/internal/utils"
+	"govault/cli"
+	"log"
 )
 
 func main() {
-	// Init DB
-	db := utils.ConnectDb()
-	utils.MigrateDb(db)
+	// // Init DB
+	// db := utils.ConnectDb()
+	// utils.MigrateDb(db)
 
-	masterPass := "iqbalpahlevi"
-	salt := crypto.GenerateRandomSalt()
+	// masterPass := "iqbalpahlevi"
+	// salt := crypto.GenerateRandomSalt()
 
-	sr := repository.New(db)
-	vs := service.New(*sr)
-	ar := repository.NewAuthRepo(db)
-	as := service.NewAuthService(*ar)
+	// sr := repository.New(db)
+	// vs := service.New(*sr)
+	// ar := repository.NewAuthRepo(db)
+	// as := service.NewAuthService(*ar)
 
-	// 1. Vault init
-	as.InitMasterPass(masterPass)
+	// // 1. Vault init
+	// as.InitMasterPass(masterPass)
 
-	// 2. Vault login
-	session, err := as.Login(masterPass)
-	if err != nil {
-		fmt.Println("master password is incorrect")
-		return
+	// // 2. Vault login
+	// session, err := as.Login(masterPass)
+	// if err != nil {
+	// 	fmt.Println("master password is incorrect")
+	// 	return
+	// }
+	// fmt.Println("session:\n", session)
+
+	// // 3. Vault create
+	// name := "gmail"
+	// username := "iqbalpahlevi@gmail.com"
+	// password := "malang123"
+	// note := "ini gmail dummy aja"
+	// s, _ := vs.CreateSecret(session.MasterPassword, name, username, password, note, salt)
+	// fmt.Println("create secret:\n", s)
+
+	// // 4. Vault get
+	// id := s.ID
+	// res, _ := vs.GetSecretById(session.MasterPassword, id)
+	// fmt.Println("get by id:\n", res)
+
+	// // 5. Vault list
+	// res2, _ := vs.GetAllSecrets()
+	// fmt.Println("get all:\n", res2)
+
+	// // 6. Vault delete
+	// vs.DeleteSecretById(id)
+
+	// cobra cli init
+	if err := cli.Execute(); err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println("session:\n", session)
-
-	// 3. Vault create
-	name := "gmail"
-	username := "iqbalpahlevi@gmail.com"
-	password := "malang123"
-	note := "ini gmail dummy aja"
-	s, _ := vs.CreateSecret(session.MasterPassword, name, username, password, note, salt)
-	fmt.Println("create secret:\n", s)
-
-	// 4. Vault get
-	id := s.ID
-	res, _ := vs.GetSecretById(session.MasterPassword, id)
-	fmt.Println("get by id:\n", res)
-
-	// 5. Vault list
-	res2, _ := vs.GetAllSecrets()
-	fmt.Println("get all:\n", res2)
-
-	// 6. Vault delete
-	vs.DeleteSecretById(id)
 }
